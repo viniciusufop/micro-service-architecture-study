05ff8031238aed157a0a36ab8073e105cc3c32ae
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-07 14:15:27 -0300

    criando os controllers do estoque

---
+++ estoque/src/main/java/br/com/vfs/estoque/controller/DepartmentController.java
@@ -0,0 +1,67 @@
+package br.com.vfs.estoque.controller;
+
+import br.com.vfs.estoque.dto.request.DepartmentRequest;
+import br.com.vfs.estoque.dto.response.DepartmentResponse;
+import br.com.vfs.estoque.mapper.DepartmentMapper;
+import br.com.vfs.estoque.model.DepartmentEntity;
+import br.com.vfs.estoque.service.DepartmentService;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author vinicius
+ * @version : $<br/>
+ * : $
+ * @since 07/06/19 13:58
+ */
+@RestController
+@RequestMapping(value = "/v1/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    private final DepartmentMapper departmentMapper;
+
+    @Autowired
+    public DepartmentController(final DepartmentService departmentService,
+            final DepartmentMapper departmentMapper){
+        this.departmentService = departmentService;
+        this.departmentMapper = departmentMapper;
+    }
+
+    @PostMapping
+    public DepartmentResponse save(@Valid @RequestBody DepartmentRequest departmentRequest){
+        final DepartmentEntity department = departmentMapper.departmentRequestToDepartmentEntity(departmentRequest);
+        return departmentMapper.departmentEntityToDepartmentResponse(departmentService.save(department));
+    }
+
+    @GetMapping(value = "/search")
+    public List<DepartmentResponse> findByName(@RequestParam("name") String name){
+        final List<DepartmentEntity> departments = departmentService.findByName(name);
+        if (departments != null)
+            return departments.stream().map(departmentMapper::departmentEntityToDepartmentResponse).collect(
+                    Collectors.toList());
+        return Collections.EMPTY_LIST;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") Long id){
+        departmentService.delete(id);
+    }
+
+    @GetMapping(value = "/{id}")
+    public DepartmentResponse findById(@PathVariable("id") Long id){
+        return departmentMapper.departmentEntityToDepartmentResponse(departmentService.findById(id));
+    }
+}
---
+++ estoque/src/main/java/br/com/vfs/estoque/controller/ProductController.java
@@ -0,0 +1,74 @@
+package br.com.vfs.estoque.controller;
+
+import br.com.vfs.estoque.dto.request.ProductRequest;
+import br.com.vfs.estoque.dto.response.ProductResponse;
+import br.com.vfs.estoque.mapper.ProductMapper;
+import br.com.vfs.estoque.model.ProductEntity;
+import br.com.vfs.estoque.service.ProductService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author vinicius
+ * @version : $<br/>
+ * : $
+ * @since 07/06/19 14:02
+ */
+@RestController
+@RequestMapping(value = "/v1/departments/{idDepartments}/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductController(final ProductService productService, final ProductMapper productMapper){
+        this.productService = productService;
+        this.productMapper = productMapper;
+    }
+
+    @PostMapping
+    public ProductResponse save(@PathVariable("idDepartments") Long departmentId,
+            @Valid @RequestBody ProductRequest productRequest){
+        final ProductEntity product = productMapper.productRequestToProductEntity(departmentId, productRequest);
+        return productMapper.productEntityToProductResponse(productService.save(product));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProductResponse findById(@PathVariable("idDepartments") Long departmentId,
+            @PathVariable("id") String id){
+        final ProductEntity product = productService.findById(id);
+        return productMapper.productEntityToProductResponse(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("idDepartments") Long departmentId,
+            @PathVariable("id") String id){
+        productService.delete(id);
+    }
+
+    @PostMapping(value = "/{id}/addAmount")
+    public ProductResponse addAmount(@PathVariable("idDepartments") Long departmentId,
+            @PathVariable("id") String id,
+            @RequestParam("amount") Integer amount){
+        final ProductEntity product = productService.addAmount(id, amount);
+        return productMapper.productEntityToProductResponse(product);
+    }
+
+    @PostMapping(value = "/{id}/subtractAmount")
+    public ProductResponse subtractAmount(@PathVariable("idDepartments") Long departmentId,
+            @PathVariable("id") String id,
+            @RequestParam("amount") Integer amount){
+        final ProductEntity product = productService.subtractAmount(id, amount);
+        return productMapper.productEntityToProductResponse(product);
+    }
+}
