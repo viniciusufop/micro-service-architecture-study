3017f7f01479f4ccbab2236ace696e76a88179dc
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-07 13:19:40 -0300

    criando as entidade do banco de estoque

---
+++ estoque/src/main/java/br/com/vfs/estoque/model/DepartmentEntity.java
@@ -0,0 +1,50 @@
+package br.com.vfs.estoque.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+/**
+ * @author vinicius
+ * @version : $<br/>
+ * : $
+ * @since 07/06/19 13:16
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "department")
+public class DepartmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "change_date")
+    private Date changeDate;
+
+    @Transient
+    public boolean isNew(){
+        return this.id == null;
+    }
+}
---
+++ estoque/src/main/java/br/com/vfs/estoque/model/ProductEntity.java
@@ -0,0 +1,50 @@
+package br.com.vfs.estoque.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+/**
+ * @author vinicius
+ * @version : $<br/>
+ * : $
+ * @since 07/06/19 13:16
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    @Column(name = "isbn")
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private DepartmentEntity department;
+
+    private String name;
+
+    private Integer amount;
+
+    private Double cost;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "change_date")
+    private Date changeDate;
+}
