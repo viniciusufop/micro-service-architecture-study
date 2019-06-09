1acc9df01789145caf5ca708d21753b261c49510
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-09 14:30:29 -0300

    incluindo as anotation de construtor nos responses

--- estoque/src/main/java/br/com/vfs/estoque/dto/response/DepartmentResponse.java
+++ estoque/src/main/java/br/com/vfs/estoque/dto/response/DepartmentResponse.java
@@ -1,15 +1,22 @@
 package br.com.vfs.estoque.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class DepartmentResponse {
 
     private Long id;
+
     private String name;
+
     private String description;
+
     private String changeDate;
 
 }
--- estoque/src/main/java/br/com/vfs/estoque/dto/response/ProductResponse.java
+++ estoque/src/main/java/br/com/vfs/estoque/dto/response/ProductResponse.java
@@ -1,8 +1,12 @@
 package br.com.vfs.estoque.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class ProductResponse {
