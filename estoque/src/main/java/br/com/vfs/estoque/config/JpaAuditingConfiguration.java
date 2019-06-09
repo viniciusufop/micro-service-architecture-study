8bbb83bb38019906739ab0420a79817b852ac416
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-09 15:24:49 -0300

    configurando jpa-auditing para gravar ultima alteracao

---
+++ estoque/src/main/java/br/com/vfs/estoque/config/JpaAuditingConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.vfs.estoque.config;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author vinicius
+ * @version : $<br/>
+ * : $
+ * @since 09/06/19 15:02
+ */
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfiguration {
+
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+
+        /*
+          if you are using spring security, you can get the currently logged username with following code segment.
+          SecurityContextHolder.getContext().getAuthentication().getName()
+         */
+        return () -> Optional.ofNullable("userNotIdentify");
+    }
+}
