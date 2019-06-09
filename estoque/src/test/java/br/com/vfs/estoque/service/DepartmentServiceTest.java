d1e11dcb1e7973ef12db485f69302a0a3ea7c19c
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-09 16:59:36 -0300

    teste dos servicos

---
+++ estoque/src/test/java/br/com/vfs/estoque/service/DepartmentServiceTest.java
@@ -0,0 +1,65 @@
+package br.com.vfs.estoque.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import br.com.vfs.estoque.exception.BusinessServiceException;
+import br.com.vfs.estoque.model.DepartmentEntity;
+import br.com.vfs.estoque.repository.DepartmentRepository;
+import br.com.vfs.estoque.service.impl.DepartmentServiceImpl;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class DepartmentServiceTest {
+
+    @InjectMocks
+    private DepartmentServiceImpl departmentService;
+
+    @Mock
+    private DepartmentRepository departmentRepository;
+
+    @Before
+    public void setUp(){
+        final DepartmentEntity department1 = buildDepartment1();
+        final DepartmentEntity department2 = buildDepartment2();
+        when(departmentRepository.findByNameContainingIgnoreCase("literatura"))
+                .thenReturn(Arrays.asList(department1, department2));
+        when(departmentRepository.findByName(department1.getName())).thenReturn(Optional.of(department1));
+    }
+
+    @Test
+    public void whenFindByName_thenDepartmentSholdBeFound(){
+        final String name = "literatura";
+        final List<DepartmentEntity> result = departmentService.findByName(name);
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test(expected = BusinessServiceException.class)
+    public void whenSaveWithSameName_thenFail(){
+        final DepartmentEntity department = buildDepartment1();
+        departmentService.save(department);
+    }
+
+    private DepartmentEntity buildDepartment1(){
+        return DepartmentEntity.builder()
+                .name("Literatura Infantil")
+                .description("Descrição do departamento Literatura Infantil")
+                .build();
+    }
+
+    private DepartmentEntity buildDepartment2(){
+        return DepartmentEntity.builder()
+                .name("Literatura Nacional")
+                .description("Descrição do departamento Literatura Nacional")
+                .build();
+    }
+
+}
---
+++ estoque/src/test/java/br/com/vfs/estoque/service/ProductServiceTest.java
@@ -0,0 +1,76 @@
+package br.com.vfs.estoque.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import br.com.vfs.estoque.exception.BusinessServiceException;
+import br.com.vfs.estoque.model.DepartmentEntity;
+import br.com.vfs.estoque.model.ProductEntity;
+import br.com.vfs.estoque.repository.ProductRepository;
+import br.com.vfs.estoque.service.impl.ProductServiceImpl;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ProductServiceTest {
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Before
+    public void setUp(){
+        final ProductEntity product = buildProduct();
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+    }
+
+    @Test(expected = BusinessServiceException.class)
+    public void whenSaveWithSameId_thenFail(){
+        final ProductEntity product = buildProduct();
+        productService.save(product);
+    }
+
+    @Test
+    public void whenFindById_thenReturnProduct(){
+        final ProductEntity product = buildProduct();
+        final ProductEntity result = productService.findById(product.getId());
+        assertThat(result.getName()).isEqualTo(product.getName());
+    }
+
+    @Test
+    public void whenFindByWrongId_thenReturnNull(){
+        final ProductEntity result = productService.findById("ISBN_NOT_FOUND");
+        assertThat(result).isNull();
+    }
+
+    @Test(expected = BusinessServiceException.class)
+    public void whenSubtractAmountNegative_thenFail(){
+        productService.subtractAmount("112314.123123", 11);
+    }
+
+    private ProductEntity buildProduct(){
+        return ProductEntity.builder()
+                .name("A Bota do Bode")
+                .id("112314.123123")
+                .amount(10)
+                .cost(19.99)
+                .department(buildDepartment())
+                .build();
+    }
+
+    private DepartmentEntity buildDepartment(){
+        return DepartmentEntity.builder()
+                .name("Literatura Infantil")
+                .description("Descrição do departamento Literatura Infantil")
+                .build();
+    }
+
+}
