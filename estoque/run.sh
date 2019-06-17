10ececb178bd11f280282ab74b403be2aafd3c1f
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-06-17 18:54:42 -0300

    alterando o estoque para utilizar o config server

--- estoque/run.sh
+++ estoque/run.sh
@@ -6,6 +6,12 @@ echo "********************************************************"
 while ! `nc -z database 3306`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
 
+echo "********************************************************"
+echo "Waiting for the configuration server to start on port 8080"
+echo "********************************************************"
+while ! `nc -z config-server 8080 `; do sleep 3; done
+echo ">>>>>>>>>>>> Configuration Server has started"
+
 java -Dserver.port=$SERVER_PORT \
     -Dfile.encoding=UTF-8       \
     -jar /usr/local/estoque/estoque.jar
