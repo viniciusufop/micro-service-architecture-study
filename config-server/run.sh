1594e60136b3b3c7aab02c734b2f00d8de6149a0
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-06-18 23:42:39 -0300

    associando o config server ao service discovery

--- config-server/run.sh
+++ config-server/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the service discovery to start on port 8080"
+echo "********************************************************"
+while ! `nc -z service-discovery 8080 `; do sleep 3; done
+echo ">>>>>>>>>>>> Service Discovery has started"
+
 java -Dserver.port=$SERVER_PORT \
      -Dfile.encoding=UTF-8       \
      -jar /usr/local/config-server/config-server.jar
